7f53b26c3b61d31fae9f188c86ab02a7416ca333
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-09-27 22:10:41 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+/**
+ * Command line application to generate commonly needed files for my projects.
+ * @author    Zachary K. Watkins <zwatkins.it@gmail.com>
+ * @version   1.0.0
+ * @copyright 2023 Zachary K. Watkins
+ */
