5a20f87e0beed60e5d750b755ef994dd0484e730
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-09-27 23:07:41 -0500

    Update reference

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const packageJson = JSON.parse(
+const pkg = JSON.parse(
   fs.readFileSync(path.join(__dirname, "package.json"), "utf8"),
 );
 
