e79281ce8a0768524c83b7f9476ef1664ad7dd05
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-09-27 22:36:42 -0500

    Add initial code to test the CLI app

--- index.js
+++ index.js
@@ -4,3 +4,30 @@
  * @version   1.0.0
  * @copyright 2023 Zachary K. Watkins
  */
+
+import fs from "fs";
+import path from "path";
+import readline from "readline";
+
+const HELP = `A tool for generating commonly needed files.
+  Usage: npx zmake [options]
+
+  Options:
+    -h, --help      Display this help message.
+    -v, --version   Display the current version.
+    --readme        Generate a README.md file.
+    --gitattributes Generate a .gitattributes file.
+    --php           Generate files for a PHP project.
+    --wp            Generate files for a WordPress project.
+    --laravel       Generate files for a Laravel project.
+    --react         Generate files for a React project.
+`;
+
+if (
+  process.argv.length === 2 ||
+  process.argv.includes("-h") ||
+  process.argv.includes("--help")
+) {
+  console.log(HELP);
+  process.exit(0);
+}
